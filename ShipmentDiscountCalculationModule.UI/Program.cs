efe53819e6dc8574787104b1aa75b766a72b0a6e
mantasmak <maksimaviciaus@gmail.com>
2021-03-30 20:47:37 +0200

    Initial commit.

---
+++ ShipmentDiscountCalculationModule.UI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ShipmentDiscountCalculationModule.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
